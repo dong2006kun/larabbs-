a218dc10ff1c087d5796cac22b783caf45b35fc3
vagrant <vagrant@homestead>
2019-11-28 08:32:40 +0000

    基础页面结构

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+
+    public function root()
+    {
+        return view('pages.root');
+    }
+
+
+
+
+
+
+}
